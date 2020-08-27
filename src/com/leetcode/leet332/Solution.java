2219c891222c856c863b186f8c0be56a42ba55db
TJ <975751290@qq.com>
2020-08-27 23:56:57 +0800

    add 332.重新安排行程

---
+++ src/com/leetcode/leet332/Solution.java
@@ -0,0 +1,78 @@
+package com.leetcode.leet332;
+
+import java.util.*;
+
+/**
+ * 332. 重新安排行程  --mid
+ * 给定一个机票的字符串二维数组 [from, to]，子数组中的两个成员分别表示飞机出发和降落的机场地点，对该行程进行重新规划排序。所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。
+ *
+ * 说明:
+ *
+ * 如果存在多种有效的行程，你可以按字符自然排序返回最小的行程组合。例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"] 相比就更小，排序更靠前
+ * 所有的机场都用三个大写字母表示（机场代码）。
+ * 假定所有机票至少存在一种合理的行程。
+ * 示例 1:
+ *
+ * 输入: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+ * 输出: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+ * 示例 2:
+ *
+ * 输入: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ * 输出: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+ * 解释: 另一种有效的行程是 ["JFK","SFO","ATL","JFK","ATL","SFO"]。但是它自然排序更大更靠后。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reconstruct-itinerary
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+//    private List<String> result = new ArrayList<>();
+//    public List<String> findItinerary(List<List<String>> tickets) {
+//        result.add("JFK");
+//        String temp = "JFK";
+//        int k = -1;
+//        return find(tickets, result, temp, k);
+//    }
+//
+//    private List<String> find(List<List<String>> tickets, List<String> result, String temp, int k) {
+//        for(int i=0; i<tickets.size(); i++){
+//            if(i==k){
+//                continue;
+//            }
+//            if(temp.equals(tickets.get(i).get(0))){
+//                temp = tickets.get(i).get(1);
+//                k = i;
+//                result.add(tickets.get(i).get(1));
+//                find(tickets, result, temp, k);
+//                break;
+//            }
+//        }
+//        return result;
+//    }
+
+    //欧拉回路、半回路 c+v
+    Map<String, PriorityQueue<String>> map = new HashMap<String, PriorityQueue<String>>();
+    List<String> itinerary = new LinkedList<String>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        for (List<String> ticket : tickets) {
+            String src = ticket.get(0), dst = ticket.get(1);
+            if (!map.containsKey(src)) {
+                map.put(src, new PriorityQueue<String>());
+            }
+            map.get(src).offer(dst);
+        }
+        dfs("JFK");
+        Collections.reverse(itinerary);
+        return itinerary;
+    }
+
+    private void dfs(String curr) {
+        while (map.containsKey(curr) && map.get(curr).size() > 0) {
+            String tmp = map.get(curr).poll();
+            dfs(tmp);
+        }
+        itinerary.add(curr);
+    }
+}
+//  [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
