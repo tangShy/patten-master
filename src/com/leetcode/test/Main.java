18878c6b325812fc09ed3fcc891d3ccf669602ca
TJ <975751290@qq.com>
2020-10-08 18:50:49 +0800

    add 344. 反转字符串

---
+++ src/com/leetcode/test/Main.java
@@ -0,0 +1,14 @@
+package com.leetcode.test;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        Runtime runtime = Runtime.getRuntime();
+//        System.out.println(runtime.freeMemory());
+//        System.out.println(runtime.totalMemory());
+//        System.out.println(runtime.maxMemory());
+        int[] a = new int[]{5,4,1,2,3};
+        System.out.println(Arrays.toString(a));
+    }
+}
