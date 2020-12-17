689cabfc65a5339ea082760fcf51bcd510fee0a2
TJ <975751290@qq.com>
2020-12-17 23:42:09 +0800

    add 714. 买卖股票的最佳时机含手续费

---
+++ src/com/leetcode/leet714/Solution.java
@@ -0,0 +1,65 @@
+package com.leetcode.leet714;
+
+/**
+ * 714. 买卖股票的最佳时机含手续费   --mid
+ * <p>
+ * 给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；非负整数 fee 代表了交易股票的手续费用。
+ * <p>
+ * 你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+ * <p>
+ * 返回获得利润的最大值。
+ * <p>
+ * 注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: prices = [1, 3, 2, 8, 4, 9], fee = 2
+ * 输出: 8
+ * 解释: 能够达到的最大利润:
+ * 在此处买入 prices[0] = 1
+ * 在此处卖出 prices[3] = 8
+ * 在此处买入 prices[4] = 4
+ * 在此处卖出 prices[5] = 9
+ * 总利润: ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+ * 注意:
+ * <p>
+ * 0 < prices.length <= 50000.
+ * 0 < prices[i] < 50000.
+ * 0 <= fee < 50000.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public static int maxProfit(int[] prices, int fee) {
+        // 动态规划
+//        int n = prices.length;
+//        int sell = 0, buy = -prices[0];
+//        for (int i = 1; i < n; ++i) {
+//            sell = Math.max(sell, buy + prices[i] - fee);
+//            buy = Math.max(buy, sell - prices[i]);
+//        }
+//        return sell;
+
+        // 贪心法
+        int n = prices.length;
+        int buy = prices[0] + fee;
+        int profit = 0;
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] + fee < buy) {
+                buy = prices[i] + fee;
+            } else if (prices[i] > buy) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1, 3, 7, 5, 10, 3};
+        int fee = 3;
+        System.out.println(maxProfit(prices, fee));
+    }
+}
