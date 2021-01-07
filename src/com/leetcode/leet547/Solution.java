0904f273b3d22f2d2432daf5753cc75b457d04ed
TJ <975751290@qq.com>
2021-01-07 22:56:03 +0800

    add 547. 省份数量

---
+++ src/com/leetcode/leet547/Solution.java
@@ -0,0 +1,64 @@
+package com.leetcode.leet547;
+
+/**
+ * 547. 省份数量        --mid
+ * <p>
+ * 有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
+ * <p>
+ * 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+ * <p>
+ * 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
+ * <p>
+ * 返回矩阵中 省份 的数量。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+ * 输出：2
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+ * 输出：3
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 200
+ * n == isConnected.length
+ * n == isConnected[i].length
+ * isConnected[i][j] 为 1 或 0
+ * isConnected[i][i] == 1
+ * isConnected[i][j] == isConnected[j][i]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-provinces
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        //深度优先遍历
+        int ans = 0;
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                ans++;
+                dfs(isConnected, visited, i);
+            }
+        }
+        return ans;
+    }
+
+    private void dfs(int[][] isConnected, boolean[] visited, int i) {
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(isConnected, visited, j);
+            }
+        }
+    }
+}
