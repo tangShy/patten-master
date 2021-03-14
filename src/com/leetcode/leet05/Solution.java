dce759f85a725809e1a68bac197aaddb98ee61ef
tang <975751290@qq.com>
2021-03-14 10:42:31 +0800

    add 中心扩散算法

--- src/com/leetcode/leet05/Solution.java
+++ src/com/leetcode/leet05/Solution.java
@@ -36,7 +36,8 @@ package com.leetcode.leet05;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution {
-    public String longestPalindrome(String s) {
+    // 暴力解法
+    public static String longestPalindrome1(String s) {
         String result = "";
         for (int i = 0; i < s.length(); i++) {
             for (int j = i + 1; j <= s.length(); j++) {
@@ -51,7 +52,7 @@ public class Solution {
         return result;
     }
 
-    private boolean isReverse(String substring) {
+    private static boolean isReverse(String substring) {
         for (int i = 0; i < substring.length() / 2; i++) {
             if (substring.charAt(i) != substring.charAt(substring.length() - 1 - i)) {
                 return false;
@@ -86,8 +87,35 @@ public class Solution {
         return ans;
     }
 
-    public static void main(String[] args) {
+    // 中心扩散算法
+    public static String longestPalindrome3(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
 
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome1("ababa"));
         System.out.println(longestPalindrome2("ababa"));
+        System.out.println(longestPalindrome3("ababa"));
     }
 }
