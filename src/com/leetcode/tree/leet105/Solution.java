fa574fb4dadc2ec349bfc5dc365eddf962417c70
tang <975751290@qq.com>
2021-06-12 23:35:07 +0800

    add 根据中+前\后序遍历构造二叉树

---
+++ src/com/leetcode/tree/leet105/Solution.java
@@ -0,0 +1,58 @@
+package com.leetcode.tree.leet105;
+
+import com.leetcode.common.TreeNode;
+
+/**
+ * 105. 从前序与中序遍历序列构造二叉树     --mid
+ *
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ *
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ *
+ * 例如，给出
+ *
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 返回如下的二叉树：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd) {
+            return null;
+        }
+        // root节点对应的就是前序遍历数组的第一个值
+        int rootVal = preorder[preStart];
+        // rootVal在中序遍历中的位置
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                index = i;
+                break;
+            }
+        }
+        int leftSize = index - inStart;
+        // 先构造当前根节点
+        TreeNode root = new TreeNode(rootVal);
+        // 递归构造左右子树
+        root.left = build(preorder, preStart + 1, preStart + leftSize,
+                inorder, inStart, index - 1);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd,
+                inorder, index + 1, inEnd);
+        return root;
+    }
+}
---
+++ src/com/leetcode/tree/leet106/Solution.java
@@ -0,0 +1,57 @@
+package com.leetcode.tree.leet106;
+
+import com.leetcode.common.TreeNode;
+
+/**
+ * 106. 从中序与后序遍历序列构造二叉树   --mid
+ *
+ * 根据一棵树的中序遍历与后序遍历构造二叉树。
+ *
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ *
+ * 例如，给出
+ *
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 后序遍历 postorder = [9,15,7,20,3]
+ * 返回如下的二叉树：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+        // root节点对应的就是后序遍历数组的最后一个值
+        int rootVal = postorder[postEnd];
+        // rootVal在中序遍历中的位置
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                index = i;
+                break;
+            }
+        }
+        int leftSize = index - inStart;
+        // 构造当前根节点
+        TreeNode root = new TreeNode(rootVal);
+        root.left = build(inorder, inStart, index - 1,
+                postorder, postStart, postStart + leftSize - 1);
+        root.right = build(inorder, index + 1, inEnd,
+                postorder, postStart + leftSize, postEnd - 1);
+        return root;
+    }
+}
