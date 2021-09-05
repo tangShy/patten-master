649d2db204e53f561d6fb5d22e13f68aa016e9ab
tang <975751290@qq.com>
2021-09-05 23:32:04 +0800

    回溯

---
+++ src/com/leetcode/week3/leet22/Solution.java
@@ -0,0 +1,62 @@
+package com.leetcode.week3.leet22;
+
+/**
+ * @Author tang
+ * @Date 2021/9/5 23:16
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 22. 括号生成(中等)
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
+ * 有效括号组合需满足：左括号必须以正确的顺序闭合。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ * 示例 2：
+ *
+ * 输入：n = 1
+ * 输出：["()"]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 8
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    // 回溯算法
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    private void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append("(");
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(")");
+            backtrack(ans, cur, open, close+1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
